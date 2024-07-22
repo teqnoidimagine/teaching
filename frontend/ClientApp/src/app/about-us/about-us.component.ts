950b472d58e023525e355e3fdfbd4a19d4105133
dheerajmehta <dheeraj_fw11_310@masai.school>
2024-07-22 12:37:59 +0530

    MVC

--- frontend/ClientApp/src/app/about-us/about-us.component.ts
+++ frontend/ClientApp/src/app/about-us/about-us.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit } from '@angular/core';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent {
-
+  ngAfterViewInit() {
+    // Initialize Kendo UI DatePicker
+    (window as any).$("#datepicker").kendoDatePicker();
+  }
 }
--- frontend/ClientApp/src/app/home/home.component.ts
+++ frontend/ClientApp/src/app/home/home.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  ngAfterViewInit() {
+    // Initialize Kendo UI DatePicker
+    (window as any).$("#datepicker").kendoDatePicker();
+  }
 }
 
